fe0012d65d1521421632fd62d817d8f8853ac981
microkhaledshafi <microkhaledshafi@yahoo.com>
2026-07-17 14:52:49 +0530

    Update billing.js

--- js/billing.js
+++ js/billing.js
@@ -1,1 +1,65 @@
+// ========================================
+// Genius Scientific ERP
+// billing.js
+// ========================================
 
+let invoiceItems = [];
+
+window.addEventListener("load", () => {
+
+    initialiseBilling();
+
+});
+
+async function initialiseBilling() {
+
+    await loadInvoiceCustomers();
+
+    generateInvoiceNumber();
+
+    document.getElementById("invoiceDate").value =
+        new Date().toISOString().split("T")[0];
+
+}
+
+async function loadInvoiceCustomers() {
+
+    const { data, error } = await supabaseClient
+        .from("Customers")
+        .select("*")
+        .order("customer_name");
+
+    if (error) {
+
+        console.error(error);
+
+        return;
+
+    }
+
+    const select = document.getElementById("invoiceCustomer");
+
+    select.innerHTML =
+        '<option value="">Select Customer</option>';
+
+    data.forEach(customer => {
+
+        select.innerHTML += `
+            <option value="${customer.id}">
+                ${customer.customer_name}
+            </option>
+        `;
+
+    });
+
+}
+
+function generateInvoiceNumber() {
+
+    const invoiceNo =
+        "INV-" + Date.now().toString().slice(-6);
+
+    document.getElementById("invoiceNumber").value =
+        invoiceNo;
+
+}
