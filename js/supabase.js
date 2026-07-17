f3d300edeb94e827b6c553c0580f3a5274f6fa05
microkhaledshafi <microkhaledshafi@yahoo.com>
2026-07-17 17:40:16 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -5,7 +5,7 @@
 
 // Replace with your own values
 const SUPABASE_URL = "https://cxssryhesrgomcdxddwn.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_ANON_KEY = "sb_publishable_QiNQzM7U5l6mJpbHIN4UJQ_nv_90lED";
 
 //==========================================
 // Create Supabase Client
